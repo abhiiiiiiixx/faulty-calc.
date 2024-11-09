5993637bdd39af3d975c5fca4fa9f7ae0a262759
Abhijeet kumar shah <129621254+abhiiiiiiixx@users.noreply.github.com>
2024-11-09 19:23:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+
+
+ let a = Math.floor(Math.random()*100)
+
+
+
+    function add(x , y ){
+        if  (a < 10){
+        console.log(x-y)}
+        else {
+            console.log(x+y)
+        }}
+    function guna(x , y ){
+        if  (a < 10){
+        console.log(x+y)}
+        else {
+            console.log(x*y)
+        }}
+    function minus(x , y ){
+        if  (a < 10){
+        console.log(x/y)}
+        else {
+            console.log(x-y)
+        }}
+    function bhag(x , y ){
+        if  (a < 10){
+        console.log(x**y)}
+        else {
+            console.log(x/y)
+        }}
+        
+
+        
+ add(2,2)
+        
+        
+        
